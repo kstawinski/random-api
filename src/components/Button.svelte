cd6e8dcb1a83f1fbb539c700385b9c366a298d6f
kstawinski <stawinskidev@gmail.com>
2020-08-22 21:05:39 +0200

    Create Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,29 @@
+<script>
+export let text;
+</script>
+
+<button class="button">
+{text}
+</button>
+
+<style type="text/scss">
+.button {
+  font-size: 16px;
+  background: #000000;
+  border: 0;
+  color: #FFFFFF;
+  font-weight: 500;
+  padding: 15px 20px;
+  box-shadow: 0 0 15px rgba(#000, 0.1);
+  cursor: pointer;
+  transition: opacity .3s, transform .2s;
+  outline: 0;
+
+  &:hover {
+    opacity: .85;
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+}
+</style>

9855058ef6b855c64c3e45c2d7158be6865a7bb3
kstawinski <stawinskidev@gmail.com>
2020-09-21 10:09:23 +0200

    Show more information about API

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,7 +6,12 @@ export let data;
   <h2 class="card__title">{data.API}</h2>
   <p class="card__description">{data.Description}</p>
 
-  <a href={data.Link} class="card__link" target="_blank">{data.Link}</a>
+  <div class="card__details">
+    <input type="checkbox" checked={ data.HTTPS === true } disabled> HTTPS <br />
+    <input type="checkbox" checked={ data.Cors === 'yes' } disabled> Cors <br />
+    <div>[ { data.Category } ]</div>
+    <a href={data.Link} class="card__link" target="_blank">{data.Link}</a>
+  </div>
 </article>
 
 <style type="text/scss">
